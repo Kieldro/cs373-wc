513e3796b992be73b733863e4bbbe855753a5683
Glenn Downing <downing@cs.utexas.edu>
2012-06-06 22:03:27 -0500

    another commit

---
+++ quizzes/Quiz1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #1 (9 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. List any piece of advice.
+   [The Harvard Guide to Collegiate Happiness]
+   (3 pts)
+
+meet the faculty
+take a mix of courses
+study in groups
+write, write, write
+speak another language
+manage time
+participate in extracurricular activities
+"""
+
+""" ----------------------------------------------------------------------
+2. List any piece of advice.
+   [Advice for Computer Science College Students]
+   (3 pts)
+
+write
+learn C
+learn microeconomics
+excel in non-CS classes
+take programming-intensive classes
+stop worrying about jobs going to India
+get a good summer internship
+"""
+
+""" ----------------------------------------------------------------------
+3. Show the cycle for 3.
+   What is the cycle length?
+   [Collatz]
+   (3 pts)
+
+3, 10, 5, 16, 8, 4, 2, 1
+8
+"""
