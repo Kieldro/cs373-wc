b13bf85116bb9d80fd544e9c22467e7109e1e8a3
Glenn Downing <downing@cs.utexas.edu>
2012-06-19 22:31:03 -0500

    another commit

---
+++ examples/Sum.hs
@@ -0,0 +1,34 @@
+-- ------
+-- Sum.hs
+-- ------
+
+{-
+(>>)     :: IO a          -> IO b     -> IO b -- 'then' operator
+(+)      :: a             -> a        -> a
+assert   :: Bool          -> a        -> a
+foldl    :: (a -> b -> a) -> a -> [b] -> a
+putStrLn :: String                    -> IO ()
+return   :: a                         -> IO a
+sum      :: [a]                       -> a
+-}
+
+import Control.Exception (assert)
+
+sum_1 :: Num a => [a] -> a
+sum_1 []       = 0
+sum_1 (x : xs) = x + sum_1 xs
+
+sum_2 :: Num a => [a] -> a
+sum_2 a = foldl (+) 0 a
+
+main :: IO ()
+main =
+    putStrLn "Sum.hs" >>
+
+    let a :: [Int]
+        a = [2,  3,  4]
+    in assert ((sum_1 a) == 9) return () >>
+       assert ((sum_2 a) == 9) return () >>
+       assert ((sum   a) == 9) return () >>
+
+    putStrLn "Done."
