64d904c8587da81d5703d4c6e375ea20ebd5666b
Glenn Downing <downing@cs.utexas.edu>
2012-06-20 19:02:29 -0500

    another commit

--- examples/Test.hs
+++ examples/Test.hs
@@ -1,22 +1,17 @@
-import Control.Exception (assert)
-
 main :: IO ()
 main = do
     putStrLn "Operators.hs"
 
     (
-    let i, j :: Int
-        i = 2
-        j = -i                 -- negation
-    assert (j == -2) return ()
+    let s :: String
+        s = "abc"
+    putStrLn s
     )
 
     (
-    let i, j, k :: Int
-        i = 2
-        j = 3
-        k = i + j             -- addition: (+) i j
-    assert (k == 5) return ()
+    let s :: String
+        s = "def"
+    putStrLn s
     )
 
     putStrLn "Done."
