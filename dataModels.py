ed2eff31bf0ec8b365daaec00f02076aa58541c9
robby35us <callmeelasinthe@gmail.com>
2012-06-29 00:23:42 -0500

    Updated dataModels.py to take full advantage of the data specification allowed by the schema.

--- dataModels.py
+++ dataModels.py
@@ -1,96 +1,64 @@
-import datetime
 from google.appengine.ext import *
 #from google.appengine.api import users
 '''
 A model class describes a kind of entity.
 '''
 
-
 class Crisis(db.Model):
 	# entity properties
-	id = IntegerProperty
+	ID = db.IntegerProperty(required=True)
 	name = db.StringProperty(required=True)
 	kind = db.StringProperty(required=True)
 	location = db.StringProperty(required=True)
 	state = db.StringProperty()
 	city = db.StringProperty()
 	country = db.StringProperty()
-	image = db.ListProperty(db.StringProperty(required=True) )
-	video = db.ListProperty(db.StringProperty(required=True) )
-	network = db.ListProperty(db.StringProperty(required=True) )
-	link = db.ListProperty(db.StringProperty(required=True) )
+	image = db.StringListProperty(required=True)
+	video = db.StringListProperty(required=True)
+	network = db.StringListProperty(required=True)
+	link = db.StringListProperty(required=True)
+	date = db.StringProperty(required=True)
+	startDate = db.DateProperty()
+	endDate = db.DateProperty()
+	additional = db.StringPeoperty()
 	humanImpact = db.StringProperty(required=True)
 	ecoImpact = db.StringProperty(required=True)
 	resources = db.StringProperty(required=True)
-	waysToHelp = db.StringProperty(required=True)
-	date = db.DateProperty(required=True)
-	time = db.TimeProperty()
-	
-	orgs = db.ListProperty(IntegerProperty)
-	people = db.ListProperty(IntegerProperty)
+	waysToHelpText = db.StringListProperty(required=True)
+	waysToHelpLinks = db.StringListProperty(required=True)
+	orgs = db.ListProperty(int)
+	people = db.ListProperty(int)
 
 class Organization(db.Model):
-	id = IntegerProperty
+	ID = IntegerProperty(require=True)
 	name = db.StringProperty(required=True)
 	kind = db.StringProperty(required=True)
 	location = db.StringProperty(required=True)
 	state = db.StringProperty()
 	city = db.StringProperty()
 	country = db.StringProperty()
-	image = db.StringProperty(required=True)
-	video = db.StringProperty(required=True)
-	network = db.StringProperty(required=True)
-	link = db.StringProperty(required=True)
+	image = db.StringListProperty(required=True)
+	video = db.StringListProperty(required=True)
+	network = db.StringListProperty(required=True)
+	link = db.StringListProperty(required=True)
 	history = db.StringProperty(required=True)
-	contactInfo = db.StringProperty(required=True)
-	
-	crisis = db.ListProperty(IntegerProperty)
-	people = db.ListProperty(IntegerProperty)
+	contactInfoText = db.StringListProperty(required=True)
+	contactInfoLinks = db.StringListProperty(required=True)
+	crisis = db.ListProperty(int)
+	people = db.ListProperty(int)
 
 class Person(db.Model):
-	id = IntegerProperty
+	ID = IntegerProperty(required=True)
 	name = db.StringProperty(required=True)
 	kind = db.StringProperty(required=True)
 	location = db.StringProperty(required=True)
 	state = db.StringProperty()
 	city = db.StringProperty()
 	country = db.StringProperty()
-	image = db.StringProperty(required=True)
-	video = db.StringProperty(required=True)
-	network = db.StringProperty(required=True)
-	link = db.StringProperty(required=True)
+	image = db.StringListProperty(required=True)
+	video = db.StringListProperty(required=True)
+	network = db.StringListProperty(required=True)
+	link = db.StringListProperty(required=True)
+	orgs = db.ListProperty(int)
+	people = db.ListProperty(int)
 	
-	orgs = db.ListProperty(IntegerProperty)
-	people = db.ListProperty(IntegerProperty)
-	
-
-
-# create an entity
-c = Crisis(name="Fukushima",
-			role="nulcear",
-			location="Japan",
-			image="NA",
-			video="NA",
-			network="NA",
-			link="NA",
-			humanImpact="great",
-			ecoImpact="vast",
-			resources="food",
-			waysToHelp="yes",
-			date="Japan")
-
-c.date = datetime.datetime.now().date()
-
-# store in the datastore
-c.put()
-
-training_registration_list = [users.User("Alfred.Smith@example.com"),
-								users.User("jharrison@example.com"),
-								users.User("budnelson@example.com")]
-# GQL query to the datastore
-employees_trained = db.GqlQuery("SELECT * FROM Crisis WHERE kind IN :1",
-								training_registration_list)
-
-for d in employees_trained:
-	d.new_hire_training_completed = 'BOOYAKASHA'
-	db.put(d)
