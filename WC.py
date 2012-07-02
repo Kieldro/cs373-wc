3d6ce2aa81978b05037bc30bfc2d6b22c4c15202
Jacob Wilke <jake.wilke@gmail.com>
2012-07-01 21:44:57 -0500

    gaeunit test quickfix

--- WC.py
+++ WC.py
@@ -3,12 +3,22 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('<meta http-equiv="Refresh" content="1;url=http://jwilke-cs373-wc.appspot.com/staticpages/splash.html">')
+        self.response.out.write('<meta http-equiv="Refresh" content="1;url=staticpages/splash.html">')
 
 class ImportPage(webapp.RequestHandler):
     def post(self):
-        test = self.request.get("data")
-	self.response.out.write(str(test))
+        xmlfile = self.request.get("data")
+        """xmlschema = open('/staticfiles/WC1.xsd', 'r')
+	try:
+            domTreeWrapper = pyxsval.parseAndValidate(xmlfile, xsdFile=xmlschema)
+        except pyxsval.XsvalError, errstr:
+            print errstr
+            print "Invalid xml file"
+        except GenXmlIfError, errstr:
+            print errstr
+            print "Parsing aborted!" """
+	self.response.headers['Content-Type'] = 'text/xml'
+	self.response.out.write(str(xmlfile))
 
     def get(self):
         self.response.out.write('Hello import')
@@ -17,8 +27,7 @@ class ExportPage(webapp.RequestHandler):
     def get(self):
         self.response.out.write('hello world')
 
-application = webapp.WSGIApplication(
-                                     [('/', MainPage), ('/xml/export', ExportPage), ('/xml/import', ImportPage)],
+application = webapp.WSGIApplication([('/', MainPage), ('/xml/export', ExportPage), ('/xml/import', ImportPage)],
                                      debug=True)
 
 def main():
