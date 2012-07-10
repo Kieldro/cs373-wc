94baa65b3b949e0b9a465a55fdc5dc343f122746
Jacob Wilke <jake.wilke@gmail.com>
2012-07-10 16:30:18 -0500

    working version of import

--- WC.py
+++ WC.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.db import delete
-from Models import Crisis, Organization, Person
+from Models import *
 from google.appengine.ext.webapp.util import run_wsgi_app
 from RunExport import runExport
 from RunImport import runImport
@@ -9,6 +9,24 @@ from genxmlif import GenXmlIfError
 
 import StringIO
 
+def deleteModels() :
+	delete(HumanImpact.all(keys_only=True))
+	delete(EconomicImpact.all(keys_only=True))
+	delete(Date.all(keys_only=True))
+	delete(Reference.all(keys_only=True))
+	delete(FullAddress.all(keys_only=True))
+	delete(Contacts.all(keys_only=True))
+	delete(Location.all(keys_only=True))
+	delete(Link.all(keys_only=True))
+	delete(ReferenceLinks.all(keys_only=True))
+	delete(Impact.all(keys_only=True))
+	delete(OrgInfo.all(keys_only=True))
+	delete(CrisisInfo.all(keys_only=True))
+	delete(PersonInfo.all(keys_only=True))
+	delete(Organization.all(keys_only=True))
+	delete(Crisis.all(keys_only=True))
+	delete(Person.all(keys_only=True))
+
 class MainPage(webapp.RequestHandler):
 	def get(self):
 		self.response.out.write('<meta http-equiv="Refresh" content="1;url=staticpages/splash.html">')
@@ -16,14 +34,13 @@ class MainPage(webapp.RequestHandler):
 class ImportPage(webapp.RequestHandler):
 	def post(self):
 		xmlfile = self.request.get("data")
-		delete(Crisis.all(keys_only=True))
-		delete(Organization.all(keys_only=True))
-		delete(Person.all(keys_only=True))
-		try:
+		deleteModels()
+		runImport(xmlfile)
+		"""try:
 			runImport(xmlfile)
 			self.response.out.write('<meta http-equiv="Refresh" content="1;url=/staticpages/port.html">')
 		except:
-			self.response.out.write("XML file does not conform to the schema.")
+			self.response.out.write("XML file does not conform to the schema.")"""
 
 	def get(self):
 		print "XML file does not conform to the schema."
@@ -33,6 +50,7 @@ class ExportPage(webapp.RequestHandler):
 		result = runExport()
 		self.response.headers['Content-Type'] = 'text/xml'
 		self.response.out.write(str(result))
+
 
 application = webapp.WSGIApplication([('/', MainPage), ('/xml/export', ExportPage),
 	('/xml/import', ImportPage)], debug=True)
