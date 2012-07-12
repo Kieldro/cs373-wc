8e6ab22ba371d512f2e1dde968558240a66a5bc1
Robert Reed <callmeelasinthe@gmail.com>
2012-07-12 08:46:58 -0500

    "Fixed" Export and removed "required" attribute from string and text properties in Models.py

--- Export.py
+++ Export.py
@@ -12,6 +12,7 @@ from xml.etree.ElementTree import Element, SubElement
 from Models import *
 from google.appengine.ext.db import Property
 
+
 # ---------
 # buildTree
 # ---------
@@ -42,6 +43,7 @@ return the root element of the data tree containing all the data
 
 	return root
 
+
 # ---------------
 # buildCrisisPage
 # ---------------
@@ -57,9 +59,11 @@ returns a list of sub-elements to be added in correct order
 	buildInitInfo(crisisElement, crisisModel);
 	buildCrisisInfo(crisisElement, crisisModel.crisisinfo)
 	buildExternalRefs(crisisElement, crisisModel.reflink)
-	SubElement(crisisElement, tag="misc", Text=crisisModel.misc)
+	temp = SubElement(crisisElement, tag="misc")
+	temp.text = crisisModel.misc
 	buildPageRefs(crisisElement, crisisModel.orgref)
 	buildPageRefs(crisisElement, crisisModel.personref)
+
 
 # ------------
 # buildOrgPage
@@ -76,9 +80,11 @@ returns a list of sub-elements to be added in correct order
 	buildInitInfo(orgElement, orgModel);
 	buildOrgInfo(orgElement, orgModel.orginfo)
 	buildExternalRefs(orgElement, orgModel.reflink)
-	SubElement(orgElement, tag="misc", text=orgModel.misc)
+	temp = SubElement(orgElement, tag="misc")
+	temp.text = orgModel.misc
 	buildPageRefs(orgElement, orgModel.crisisref)
 	buildPageRefs(orgElement, orgModel.personref)
+
 
 # ---------------
 # buildPersonPage
@@ -95,9 +101,11 @@ returns a list of sub-elements to be added in correct order
 	buildInitInfo(personElement, personModel)
 	buildPersonInfo(personElement, personModel.personinfo)
 	buildExternalRefs(personElement, personModel.reflink)
-	SubElement(personElement, tag="misc", text=personModel.misc)
+	temp = SubElement(personElement, tag="misc")
+	temp.text = personModel.misc
 	buildPageRefs(personElement, personModel.crisisref)
 	buildPageRefs(personElement, personModel.orgref)
+
 
 # -------------
 # buildInitInfo
@@ -105,7 +113,9 @@ returns a list of sub-elements to be added in correct order
 
 def buildInitInfo(element, model):	
 	element.attrib["id"] =  model.ID
-	SubElement(element, tag="name", text=model.name)
+	temp = SubElement(element, tag="name")
+	temp.text = model.name
+
 
 # ---------------
 # buildCrisisInfo
@@ -113,92 +123,166 @@ def buildInitInfo(element, model):
 
 def buildCrisisInfo(crisisElement, crisisInfoModel):
 	info = Element(tag="info")
-	SubElement(info, tag="histroy", text=crisisInfoModel.history)
-	SubElement(info, tag="help", text=crisisInfoModel.helps)
-	SubElement(info, tag="resources", text=crisisInfoModel.resources)
-	SubElement(info, tag="type", text=crisisInfoModel.ctype)
+	temp = SubElement(info, tag="history")
+	temp.text = crisisInfoModel.history
+	temp = SubElement(info, tag="help")
+	temp.text = crisisInfoModel.helps
+	temp = SubElement(info, tag="resources")
+	temp.text = crisisInfoModel.resources
+	temp = SubElement(info, tag="type")
+	temp.text = crisisInfoModel.ctype
 	buildDate(info, crisisInfoModel.date, 'time')
 	buildLocation(info, crisisInfoModel.location)
 	buildImpact(info, crisisInfoModel.impact)
 	crisisElement.append(info)
 
+
+# ---------------
+# buildDate
+# ---------------
+
 def buildDate(parentElem, dateModel, title):
 	time = Element(tag=title)
-	SubElement(time, tag="time", text=dateModel.time)
-	SubElement(time, tag="day", text=str(dateModel.day))
-	SubElement(time, tag="month", text=str(dateModel.month))
-	SubElement(time, tag="year", text=str(dateModel.year))
-	SubElement(time, tag="misc", test=dateModel.time_misc)
+	temp = SubElement(time, tag="time")
+	temp.text = dateModel.time
+	temp = SubElement(time, tag="day")
+	temp.text = str(dateModel.day)
+	temp = SubElement(time, tag="month")
+	temp.text = str(dateModel.month)
+	temp = SubElement(time, tag="year")
+	temp.text = str(dateModel.year)
+	temp = SubElement(time, tag="misc")
+	temp.text = str(dateModel.misc)
 	parentElem.append(time)
+
+
+# ---------------
+# buildLocation
+# ---------------
 
 def buildLocation(parentElem, locationModel):
 	loc = Element(tag="loc")
-	SubElement(loc, tag="city", text=locationModel.city)
-	SubElement(loc, tag="region", text=locationModel.region)
-	SubElement(loc, tag="country", text=locationModel.country)
+	temp = SubElement(loc, tag="city")
+	temp.text = locationModel.city
+	temp = SubElement(loc, tag="region")
+	temp.text = locationModel.region
+	temp = SubElement(loc, tag="country")
+	temp.text = locationModel.country
 	parentElem.append(loc)
+	
+	
+# ---------------
+# buildImpact
+# ---------------
 
 def buildImpact(parentElem, impactModel):
 	impact = Element(tag="impact")
 	buildHumanImpact(impact, impactModel.human_impact)
 	buildEconomicImpact(impact, impactModel.eco_impact)
 	parentElem.append(impact)
-	
+
+
+# ---------------
+# buildHumanImpact
+# ---------------
+
 def buildHumanImpact(parentElem, hImpactModel):
 	human = Element(tag="human")
-	SubElement(human, tag="deaths", text=str(hImpactModel.deaths))
-	SubElement(human, tag="displaced", text=str(hImpactModel.displaced))
-	SubElement(human, tag="injured", text=str(hImpactModel.injured))
-	SubElement(human, tag="missing", text=str(hImpactModel.missing))
-	SubElement(human, tag="misc", test=hImpactModel.himpact_misc)
+	temp = SubElement(human, tag="deaths")
+	temp.text = str(hImpactModel.deaths)
+	temp = SubElement(human, tag="displaced")
+	temp.text = str(hImpactModel.displaced)
+	temp = SubElement(human, tag="injured")
+	temp.text = str(hImpactModel.injured)
+	temp = SubElement(human, tag="missing")
+	temp.text = str(hImpactModel.missing)
+	temp = SubElement(human, tag="misc")
+	temp.test = hImpactModel.himpact_misc
 	parentElem.append(human)
+
+
+# ---------------
+# buildEconomicImpact
+# ---------------
 
 def buildEconomicImpact(parentElem, eImpactModel):
 	eco = Element(tag="economic")
-	SubElement(eco, tag="amount", text=str(eImpactModel.amount))
-	SubElement(eco, tag="currency", text=eImpactModel.currency)
-	SubElement(eco, tag="misc", text=eImpactModel.eimpact_misc)
+	temp = SubElement(eco, tag="amount")
+	temp.text = str(eImpactModel.amount)
+	temp = SubElement(eco, tag="currency")
+	temp.text = str(eImpactModel.amount)
+	temp = SubElement(eco, tag="misc")
+	temp.text = eImpactModel.eimpact_misc
 	parentElem.append(eco)
-	
+
+
 # ---------------
 # buildOrgInfo
 # ---------------
 
 def buildOrgInfo(orgElement, orgInfoModel):
 	info = Element(tag="info")
-	SubElement(info, tag="type", text=orgInfoModel.otype)
-	SubElement(info, tag="history", text=orgInfoModel.history)
+	temp = SubElement(info, tag="type")
+	temp.text = orgInfoModel.otype
+	temp = SubElement(info, tag="history")
+	temp.text = orgInfoModel.otype
 	buildContact(info, orgInfoModel.contacts)
 	buildLocation(info, orgInfoModel.location)
 	orgElement.append(info)
 
+
+# ---------------
+# buildContact
+# ---------------
+
 def buildContact(parentElem, contactModel):
 	contact = Element(tag="contact")	
-	SubElement(contact, tag="phone", text=contactModel.phone)
-	SubElement(contact, tag="email", text=contactModel.email)
-  	buildAddress(contact, contactModel.address)
+	temp = SubElement(contact, tag="phone")
+	temp.text = contactModel.phone
+	temp = SubElement(contact, tag="email")
+	temp.text = contactModel.email
+	buildAddress(contact, contactModel.address)
 	parentElem.append(contact)
+
+
+# ---------------
+# buildAddress
+# ---------------
 
 def buildAddress(parentElem, addressModel):
 	mail = Element(tag="mail")	
-	SubElement(mail, tag="address", text=addressModel.address)
-	SubElement(mail, tag="city", text=addressModel.city)
-	SubElement(mail, tag="state", text=addressModel.state)
-	SubElement(mail, tag="country", text=addressModel.country)
-	SubElement(mail, tag="zip", text=addressModel.zipcode)
+	temp = SubElement(mail, tag="address")
+	temp.text = addressModel.address
+	temp = SubElement(mail, tag="city")
+	temp.text = addressModel.city
+	temp = SubElement(mail, tag="state")
+	temp.text = addressModel.state
+	temp = SubElement(mail, tag="country")
+	temp.text = addressModel.country
+	temp = SubElement(mail, tag="zip")
+	temp.text = addressModel.zipcode
 	parentElem.append(mail)
-	
+
+
 # ---------------
 # buildPersonInfo
 # ---------------
 
 def buildPersonInfo(personElement, personModel):
 	info = Element(tag="info")
-	SubElement(info, tag="type", text=personModel.ptype)	
+	temp = SubElement(info, tag="type")	
+	temp.text = personModel.ptype
 	buildDate(info, personModel.birthdate, 'birthdate')
-	SubElement(info, tag="nationality", text=personModel.nationality)
-	SubElement(info, tag="biography", text=personModel.biography)
+	temp = SubElement(info, tag="nationality")
+	temp.text = personModel.nationality
+	temp = SubElement(info, tag="biography")
+	temp.text = personModel.biography
 	personElement.append(info)
+
+
+# ---------------
+# buildRefs
+# ---------------
 
 def buildRefs(elem, refs):
 	for ref in refs:
@@ -206,6 +290,11 @@ def buildRefs(elem, refs):
 		temp = Element(tag=ref.link_type)
 		buildLink(temp, ref.site, ref.title, ref.url, ref.description)
 		elem.append(temp)
+
+
+# ---------------
+# buildExternalRefs
+# ---------------
 
 def buildExternalRefs(element, model):
 	ref = Element(tag="ref")
@@ -216,11 +305,26 @@ def buildExternalRefs(element, model):
 	buildRefs(ref, model.ext)
 	element.append(ref)
 
+
+# ---------------
+# buildLink
+# ---------------
+
 def buildLink(element, site, title, url, description):
-	SubElement(element, tag="site", text=site)
-	SubElement(element, tag="title", text=title)
-	SubElement(element, tag="url", text=url)
-	SubElement(element, tag="description", text=description)
+	temp = SubElement(element, tag="site")
+	temp.text = site
+	temp = SubElement(element, tag="title")
+	temp.text = title
+	temp = SubElement(element, tag="url")
+	temp.text = url
+	if description != None:
+		temp = SubElement(element, tag="description")
+		temp.text = description
+
+
+# ---------------
+# buildPageRefs
+# ---------------
 
 def buildPageRefs(element, refs):
 	for ref in refs:
