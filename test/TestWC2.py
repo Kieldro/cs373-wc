7c98c38c7e90ad168f3a62d29676b58bce60717f
Robert Reed <callmeelasinthe@gmail.com>
2012-07-12 14:25:02 -0500

    Started making xml strings for testing

--- test/TestWC2.py
+++ test/TestWC2.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import unittest
 from Models import *
 from Import import *
+from Export import *
 from xml.etree.ElementTree import Element, SubElement, parse
 import StringIO
 #from google.appengine.ext import db
@@ -8,21 +10,211 @@ import StringIO
 
 
 class Success(unittest.TestCase):
-	def test_success(self):
-		self.assertTrue(True)
+
+  
+	humanImpactOpen1 = """<human>
+					<deaths>-1</deaths>
+					<displaced>-1</displaced>
+					<injured>-1</injured>
+					<missing>-1</missing>"""
+					
+	humanImpactOpen2 = """<human>
+					<deaths>-1</deaths>
+					<displaced>-1</displaced>
+					<injured>-1</injured>
+					<missing>-1</missing>"""
 	
-	# import tests
-	
-	def test_import(self):
-		xmlstring = StringIO.StringIO(filestring)
-		tree = parse(xmlstring)
+	humanImpactOpen3 = """<human>
+					<deaths>-1</deaths>
+					<displaced>-1</displaced>
+					<injured>-1</injured>
+					<missing>-1</missing>"""
+	humanImpactClose = """</human>"""
+			
+	ecoImpactOpen1 = """<economic>
+					<amount>12039847</amount>
+					<currency>RESEARCH burritos</currency>"""
+	ecoImpactOpen1 = """<economic>
+					<amount>23</amount>
+					<currency></currency>"""
+	ecoImpactOpen1 = """<economic>
+					<amount>0</amount>
+					<currency>Yen</currency>"""
+	ecoimpactClose = """</economic>"""
+
+	misc1 = """<misc>research</misc>"""
+	misc2 = """<misc>single Ladies
+
+All the single ladies (7x)
+
+Now put your hands up
+Up in the club, we just broke up
+I’m doing my own little thing
+you Decided to dip but now you wanna trip
+Cuz another brother noticed me
+I’m up on him, he up on me
+dont pay him any attention
+cuz i cried my tears, GAVE three good years
+Ya can’t be mad at me
+
+[Chorus]
+Cuz if you liked it then you should have put a ring on it
+If you liked it then you shoulda put a ring on it
+Don’t be mad once you see that he want it
+If you liked it then you shoulda put a ring on it
+
+wo oh ooh oh oh ooh oh oh ooh oh oh oh x2
+
+
+(Chorus)Cuz if you liked it then you should have put a ring on it
+If you liked it then you shoulda put a ring on it
+Don’t be mad once you see that he want it
+If you liked it then you shoulda put a ring on it
+
+
+I got gloss on my lips, a man on my hips
+hold me tighter than my Dereon jeans
+acting up, drink in my cup
+I couldnt care less what you think
+I need no permission, did I mention
+Dont pay him any attention
+Cuz you had your turn
+But now you gonna learn
+What it really feels like to miss me
+
+(Chorus)
+
+Cuz if you liked it then you should have put a ring on it
+If you liked it then you shoulda put a ring on it
+Don’t be mad once you see that he want it
+If you liked it then you shoulda put a ring on it
+
+woo oh ooh oh oh ooh oh oh ooh oh oh oh 2x
+
+Don’t treat me to the things of this world
+I’m not that kind of girl
+Your love is what I prefer, what I deserve
+Is a man that makes me, then takes me
+And delivers me to a destiny, to infinity and beyond
+Pull me into your arms
+Say I’m the one you WANT
+If you don’t, you’ll be alone
+And like a ghost I’ll be gone
+
+All the single ladies (7x)
+Now put your hands up
+woo oh ooh oh oh ooh oh oh ooh
+oh oh oh 2x
+
+Cuz if you liked it then you should have put a ring on it
+If you liked it then you shoulda put a ring on it
+Don’t be mad once you see that he want it
+If you liked it then you shoulda put a ring on it woo oh ooh 2x</misc>"""
+	misc3 = """<misc/>"""
+
+	crisisRef1 = """<crisis idref="CA"/>"""
+	crisisRef2 = """<crisis idref="CB"/>"""
+	crisisRef3 = """<crisis idref="CC"/>"""
+	orgRef1 = """<organization idref="OA"/>"""
+	orgRef2 = """<organization idref="OB"/>"""
+	orgRef3 = """<organization idref="OC"/>"""
+	personRef1 = """<person idref="PA"/>"""
+	personRef2 = """<person idref="PB"/>"""
+	personRef3 = """<person idref="PC"/>"""
 		
-		buildModels(tree)
-		self.assertTrue(True)
+	refOpen = """<ref>"""
+	refClose = """</ref>"""
+
+	p_imageLink = """<primaryImage>
+				<site>http://stopworldhunger.webs.com</site>
+				<title>World Hunger</title>
+				<url>http://stopworldhunger.webs.com/hunger.jpg</url>
+				<description>Starving children</description> 
+			</primaryImage>"""
+			
+	imageLink1 = """<image>
+				<site>TestImage1</site>
+				<title>World Health Organization</title>
+				<url>http://www.who.int/en</url>
+				<description>World Health Organization</description>
+			</image>"""
+			
+	imageLink2 = """<image>
+				<site>TestImage2</site>
+				<title>World Hunger</title>
+				<url>http://viroquafood.coop/Portals/62289/images/hunger%20in%20america.jpg</url>
+				<description>Homeless Sign</description>
+			</image>"""
+
+	imageLink3 = """<image>
+				<site>TestImage3</site>
+				<title>World Health Organization</title>
+				<url>http://www.who.int/en</url>
+			</image>"""
+
+	videoLink1 = """<video>
+				<site>TestVideo1</site>
+				<title>World Health Organization</title>
+				<url>http://www.who.int/en</url>
+				<description>World Health Organization</description>
+			</video>"""
+			
+	videoLink2 = """<video>
+				<site>TestVideo2</site>
+				<title>World Health Organization</title>
+				<url></url>
+				<description>World Health Organization</description>
+			</video>"""
+
+	videoLink3 = """<video>
+				<site>TestVideo3</site>
+				<title>World Health Organization</title>
+				<url>http://www.who.int/en</url>
+			</video>"""
+
+	socialLink1 = """<social>
+				<site>TestSocial1</site>
+				<title>World Health Organization</title>
+				<url>http://www.who.int/en</url>
+				<description>World Health Organization</description>
+			</social>"""
+			
+	socialLink2 = """<social>
+				<site>TestSocial2</site>
+				<title>World Health Organization</title>
+				<url></url>
+				<description>World Health Organization</description>
+			</social>"""
+
+	socialLink3 = """<social>
+				<site>TestSocial3</site>
+				<title>World Health Organization</title>
+				<url>http://www.who.int/en</url>
+			</social>"""
 
 
+	extLink1 = """<ext>
+				<site>TestSite1</site>
+				<title>World Health Organization</title>
+				<url>http://www.who.int/en</url>
+				<description>World Health Organization</description>
+			</ext>"""
+			
+	extLink2 = """<ext>
+				<site>TestSite2</site>
+				<title>World Health Organization</title>
+				<url></url>
+				<description>World Health Organization</description>
+			</ext>"""
 
-filestring = '''<?xml version="1.0" ?> 
+	extLink3 = """<ext>
+				<site>TestSite3</site>
+				<title>World Health Organization</title>
+				<url>http://www.who.int/en</url>
+			</ext>"""
+
+
+	filestring = '''<?xml version="1.0" ?> 
 <worldCrises>
 	<crisis id="worldhunger">
 		<name>World Hunger</name>
@@ -63,13 +255,13 @@ filestring = '''<?xml version="1.0" ?>
 				<site>http://stopworldhunger.webs.com</site>
 				<title>World Hunger</title>
 				<url>http://stopworldhunger.webs.com/hunger.jpg</url>
-				<description>Starving children</description> <!-- OPTIONAL -->
+				<description>Starving children</description> 
 			</primaryImage>
 			<image>
 				<site>http://viroquafood.coop</site>
 				<title>Stop World Hunger</title>
 				<url>http://viroquafood.coop/Portals/62289/images/hunger%20in%20america.jpg</url>
-				<description>Homeless Sign</description> <!-- OPTIONAL -->
+				<description>Homeless Sign</description> 
 			</image>
 			<video>
 				<site>http://www.youtube.com</site>
@@ -81,37 +273,37 @@ filestring = '''<?xml version="1.0" ?>
 				<site>http://www.youtube.com</site>
 				<title>World Hunger PSA </title>
 				<url>http://www.youtube.com/watch?v=0W2Ic5hV28w</url>
-				<description>World Hunger PSA</description> <!-- OPTIONAL -->
+				<description>World Hunger PSA</description>
 			</video>
 			<social>
 				<site>http://www.facebook.com</site>
 				<title>World Hunger Relief</title>
 				<url>http://www.facebook.com/WorldHungerRelief</url>
-				<description>World Hunger Relief</description> <!-- OPTIONAL -->
+				<description>World Hunger Relief</description> 
 			</social>
 			<social>
 				<site>https://twitter.com</site>
 				<title>World Food Day </title>
 				<url>https://twitter.com/#!/FAOWFD</url>
-				<description>World Food Day</description> <!-- OPTIONAL -->
+				<description>World Food Day</description> 
 			</social>
 			<ext>
 				<site>http://www.who.int</site>
 				<title>World Health Organization</title>
 				<url>http://www.who.int/en</url>
-				<description>World Health Organization</description> <!-- OPTIONAL -->
+				<description>World Health Organization</description>
 			</ext>
 			<ext>
 				<site>http://www.worldhunger.org/</site>
 				<title>http://www.worldhunger.org/</title>
 				<url>http://www.worldhunger.org/</url>
-				<description>http://www.worldhunger.org/</description> <!-- OPTIONAL -->
+				<description>http://www.worldhunger.org/</description> 
 			</ext>
 			<ext>
 				<site>http://www.freedomfromhunger.org/</site>
 				<title>http://www.freedomfromhunger.org/</title>
 				<url>http://www.freedomfromhunger.org/</url>
-				<description>http://www.freedomfromhunger.org/</description> <!-- OPTIONAL -->
+				<description>http://www.freedomfromhunger.org/</description> 
 			</ext>
 		</ref>
 		<misc>RESEARCH</misc>
@@ -146,13 +338,13 @@ filestring = '''<?xml version="1.0" ?>
 				<site>http://stopworldhunger.webs.com</site>
 				<title>World Hunger</title>
 				<url>http://stopworldhunger.webs.com/hunger.jpg</url>
-				<description>Starving children</description> <!-- OPTIONAL -->
+				<description>Starving children</description> 
 			</primaryImage>
 			<image>
 				<site>http://viroquafood.coop</site>
 				<title>Stop World Hunger</title>
 				<url>http://viroquafood.coop/Portals/62289/images/hunger%20in%20america.jpg</url>
-				<description>Homeless Sign</description> <!-- OPTIONAL -->
+				<description>Homeless Sign</description> 
 			</image>
 			<video>
 				<site>http://www.youtube.com</site>
@@ -164,37 +356,37 @@ filestring = '''<?xml version="1.0" ?>
 				<site>http://www.youtube.com</site>
 				<title>World Hunger PSA </title>
 				<url>http://www.youtube.com/watch?v=0W2Ic5hV28w</url>
-				<description>World Hunger PSA</description> <!-- OPTIONAL -->
+				<description>World Hunger PSA</description> 
 			</video>
 			<social>
 				<site>http://www.facebook.com</site>
 				<title>World Hunger Relief</title>
 				<url>http://www.facebook.com/WorldHungerRelief</url>
-				<description>World Hunger Relief</description> <!-- OPTIONAL -->
+				<description>World Hunger Relief</description> 
 			</social>
 			<social>
 				<site>https://twitter.com</site>
 				<title>World Food Day </title>
 				<url>https://twitter.com/#!/FAOWFD</url>
-				<description>World Food Day</description> <!-- OPTIONAL -->
+				<description>World Food Day</description> 
 			</social>
 			<ext>
 				<site>http://www.who.int</site>
 				<title>World Health Organization</title>
 				<url>http://www.who.int/en</url>
-				<description>World Health Organization</description> <!-- OPTIONAL -->
+				<description>World Health Organization</description> 
 			</ext>
 			<ext>
 				<site>http://www.worldhunger.org/</site>
 				<title>http://www.worldhunger.org/</title>
 				<url>http://www.worldhunger.org/</url>
-				<description>http://www.worldhunger.org/</description> <!-- OPTIONAL -->
+				<description>http://www.worldhunger.org/</description> 
 			</ext>
 			<ext>
 				<site>http://www.freedomfromhunger.org/</site>
 				<title>http://www.freedomfromhunger.org/</title>
 				<url>http://www.freedomfromhunger.org/</url>
-				<description>http://www.freedomfromhunger.org/</description> <!-- OPTIONAL -->
+				<description>http://www.freedomfromhunger.org/</description> 
 			</ext>
 		</ref>
 		<misc>RESEARCH</misc>
@@ -278,3 +470,15 @@ filestring = '''<?xml version="1.0" ?>
 		<org idref="WHO"/>
 	</person>
 </worldCrises>'''
+
+	def test_success(self):
+		self.assertTrue(True)
+	
+	# import tests
+	
+	def test_import(self):
+		xmlstring = StringIO.StringIO(filestring)
+		tree = parse(xmlstring)
+		
+		buildModels(tree)
+		self.assertTrue(True)
