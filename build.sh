f7513cc4123112ab934518da629c660537b56354
Ian Buitrago <kieldro@gmail.com>
2012-07-24 11:16:26 -0500

    renamed files to WC3*. Deleted extraneous failes.

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 # file variables
 source="WC.py"
 
-#dev_appserver.py .
+dev_appserver.py .
 
 
 # multicomment cannot have leading spaces
 
+<<MULTICOMMENT
 export PYTHONPATH=~/cs373/WCAppspot/google_appengine/:	\
 					~/cs373/WCAppspot/google_appengine/lib/webob_1_1_1/
 echo RUNNING PYDOC...;	pydoc -w ./
 mv *.html pydoc/
-<<MULTICOMMENT
-echo GENERATING COMMIT LOG...;	git log > WC2.log
+echo GENERATING COMMIT LOG...;	git log > WC3.log
 
 echo ZIPPING FILES...
-zip WC2 README.txt pydoc/ *.py app.yaml WC2.log TestWC2.out test/TestWC2.py \
-		WC2.pdf UML.svg WC2.xml
+zip WC3 README.txt pydoc/ *.py app.yaml TestWC3.out test/TestWC3.py WC3.log \
+		WC3Presentation.pdf WC3Reort.pdf WC3.xml
 
-turnin --submit hychyc07 cs373pj4 WC2.zip
-turnin --list	hychyc07 cs373pj4 WC2.zip
-turnin --verify hychyc07 cs373pj4 WC2.zip
+turnin --submit hychyc07 cs373pj5 WC3.zip
+turnin --list	hychyc07 cs373pj5 WC3.zip
+turnin --verify hychyc07 cs373pj5 WC3.zip
 MULTICOMMENT
